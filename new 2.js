fee952a2696ed9574d1deec6fd4130ee1fa8a694
torikjaelani <35658118+torikjaelani@users.noreply.github.com>
2018-01-31 10:47:25 +0700

    Add files via upload

---
+++ new 2.js
@@ -0,0 +1,26 @@
+::-webkit-scrollbar {
+   width: 12px;
+}
+::-webkit-scrollbar-track {
+   background: #f90;
+}
+::-webkit-scrollbar-thumb {
+   background: blue;
+}
+::-webkit-scrollbar-button:start:decrement,
+::-webkit-scrollbar-button:end:increment {
+   background: black;
+}
+::-ms-scrollbar {
+   width: 12px;
+}
+::-ms-scrollbar-track {
+   background: #f90;
+}
+::-ms-scrollbar-thumb {
+   background: blue;
+}
+::-ms-scrollbar-button:start:decrement,
+::-ms-scrollbar-button:end:increment {
+   background: black;
+}
